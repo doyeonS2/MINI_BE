215db2c161ae126ce19b11aaa55115f4df4fbfc6
doyeon <1162004@naver.com>
2022-11-04 12:33:22 +0900

    도연/22.11.04

--- src/main/java/com/kh/miniservlet/servlet/signup/IdCheckServlet.java
+++ src/main/java/com/kh/miniservlet/servlet/signup/IdCheckServlet.java
@@ -1,4 +1,37 @@
 package com.kh.miniservlet.servlet.signup;
 
-public class IdCheckServlet {
+import com.kh.miniservlet.common.Common;
+import com.kh.miniservlet.dao.SignUpDAO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // 3000번에 대한 cors 허용
+public class IdCheckServlet extends HttpServlet {
+    @PostMapping("/IdCheckServlet")
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("utf-8"); // 한글 깨짐 방지를 위해서 설정
+        StringBuffer sb = Common.reqStringBuff(request); // 요청 메시지 받기
+        JSONObject jsonObj = Common.getJsonObj(sb); // 요청 받은 메시지 JSON 파싱
+
+        String getId = (String)jsonObj.get("id");
+        SignUpDAO dao = new SignUpDAO();
+
+        boolean isNotReg = dao.IdCheck(getId); // isNotReg = TRUE 가입 안된 경우
+
+
+        PrintWriter out = response.getWriter();
+        JSONObject resJson = new JSONObject();
+
+        if(isNotReg) resJson.put("result", "OK"); // 가입 안되어 있으면 OK
+        else resJson.put("result", "NOK"); // 가입되어 있으면 NOK
+        out.print(resJson);
+    }
 }
--- src/main/java/com/kh/miniservlet/vo/MemberVO.java
+++ src/main/java/com/kh/miniservlet/vo/MemberVO.java
@@ -3,12 +3,24 @@ package com.kh.miniservlet.vo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Date;
+
 @Getter
 @Setter
 public class MemberVO {
+    // 회원
     private String id; // 회원 아이디
     private String pwd; // 회원 패스워드
     private String pwdCheck; // 패스워드 확인
     private String memName; // 회원 이름
     private String email; // 회원 이메일
+    private String emailName; // @메일명
+    private String phone; // 회원 전화번호 010
+    private String phone2; // 회원 전화번호 중간 번호
+    private String phone3; // 회원 전화번호 마지막 번호
+    private String phoneNum; // 인증번호 6자리
+    private String addrNum; // 우편번호
+    private String addr1; // 주소
+    private String addr2; // 상세 주소
+    private Date regDate; // 회원 가입일, 변환해줘야 함
 }
