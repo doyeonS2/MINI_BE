cae38702f0f6514455b81769210103000a1691b5
이수빈 <sb9479@naver.com>
2022-11-05 18:16:22 +0900

    수빈/22.11.05

--- src/main/java/com/kh/miniservlet/dao/EditMemberDAO.java
+++ src/main/java/com/kh/miniservlet/dao/EditMemberDAO.java
@@ -1,12 +1,14 @@
 package com.kh.miniservlet.dao;
 
 import com.kh.miniservlet.common.Common;
+import com.kh.miniservlet.vo.BoardVO;
 import com.kh.miniservlet.vo.MemberVO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditMemberDAO {
     private Connection conn = null;
@@ -17,30 +19,54 @@ public class EditMemberDAO {
     // 로그인이 되어있는 상태면 회원정보 수정가능하도록
     // 로그인이 안되어있는 상태면 로그인하도록
     // 여기에 화면에 뿌려주기 위한 Select문이 필여함(복붙해서 셀렉트문으로 바꿔_)
-    public void MemInfo(MemberVO merberVO) {
-        String pwd = merberVO.getPwd();
-        String name = merberVO.getMemName();
-        String email = merberVO.getEmail();
-        String phone = merberVO.getPhone();
-        String addr = merberVO.getAddr();
+    public List<MemberVO> MemInfo(String id) {
+        int resState = 300; // 200은 로그인, 300은 Id없음, 400은 pwd 오류 => 이걸 정리한게 API문서
 
+        List<MemberVO> list = new ArrayList<>();
         try {
-
             conn = Common.getConnection();
             stmt = conn.createStatement(); // Statement 객체
-            String sql = "SELECT * MEM_TB WHERE ID = " + "'" + merberVO + "'";
-            rs = stmt.executeQuery(sql);
+            String sql = "SELECT * FROM MEM_TB WHERE ID = " + "'" + id + "'";
+            rs = stmt.executeQuery(sql); // select문이니까 executeQuery
 
-            stmt.executeQuery(sql); // 페이지에 뿌릴때는 stmt.executeQuery(결과값이 있고 단순히 호출할 때)
+            // id없으면 진입불가
+            while(rs.next()) { // 읽은 데이터가 있으면 true
+                String sqlId = rs.getString("ID"); // 쿼리문 수행 결과에서 ID값을 가져 옴
+                String sqlPwd = rs.getString("PASSWORD");
+                String sqlName = rs.getString("NAME");
+                String sqlEmail = rs.getString("EMAIL");
+                String sqlPhone = rs.getString("PHONE");
+                String sqlAddr = rs.getString("ADDR");
+                String sqlDate = rs.getString("REG_DATE");
 
-            pstmt.close();
-            conn.close();
 
-        } catch (Exception e) {
-            System.out.println("회원정보 수정 중 예외 발생");
-            e.printStackTrace(); //에러가 맨 위로 올라가게 해줌(에러찾기 쉽게해주는)
+                MemberVO vo = new MemberVO();
+
+
+                vo.setId(sqlId);
+                vo.setPwd(sqlPwd);
+                vo.setMemName(sqlName);
+                vo.setEmail(sqlEmail);
+                vo.setPhone(sqlPhone);
+                vo.setAddr(sqlAddr);
+                list.add(vo);
+
+                System.out.println("ID : " + sqlId);
+                System.out.println("PASSWORD : " + sqlPwd);
+                System.out.println("NAME : " + sqlName);
+
+            }
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
+
+        } catch(Exception e) {
+            e.printStackTrace();
         }
+
+        return list;
     }
+
     //회원 정보 수정
     public void editInfo(MemberVO merberVO) {
         String pwd = merberVO.getPwd();
@@ -49,10 +75,11 @@ public class EditMemberDAO {
         String phone = merberVO.getPhone();
         String addr = merberVO.getAddr();
 
+
         try {
 
             conn = Common.getConnection();
-            String sql = "UPDATE MEM_TB SET PWD = ?, NAME = ?, EMAIL = ?, PHONE = ?, ADDR = ?";
+            String sql = "UPDATE MEM_TB SET PWD = ?, NAME = ?, EMAIL = ?, PHONE = ?, ADDR = ? WHERE ID = ?";
 
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, pwd);
--- src/main/java/com/kh/miniservlet/servlet/mypage/EditMemServlet.java
+++ src/main/java/com/kh/miniservlet/servlet/mypage/EditMemServlet.java
@@ -1,9 +1,68 @@
 package com.kh.miniservlet.servlet.mypage;
 
+import com.kh.miniservlet.common.Common;
+import com.kh.miniservlet.dao.EditMemberDAO;
+import com.kh.miniservlet.vo.MemberVO;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 @RestController
-@CrossOrigin
-public class EditMemServlet {
+@CrossOrigin(origins = "http://localhost:3000") // 3000번에 대한 cors 허용
+public class EditMemServlet extends HttpServlet {
+    @PostMapping("/EditMemServlet") // 이클립스의 서블렛파일에서 쓰는 @WebServlet("/LoginServlet")와 같은 기능
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("utf-8"); // 한글 깨짐 방지를 위해서 설정
+        response.setCharacterEncoding("utf-8"); // 한글 깨짐 방지를 위해서 설정
+
+        StringBuffer sb = Common.reqStringBuff(request); // 요청 메시지 받기
+        JSONObject jsonObj = Common.getJsonObj(sb); // 요청 받은 메시지 JSON 파싱
+
+        String getId = (String)jsonObj.get("id");
+        System.out.println("받은 아이디 : " + getId);
+//        String getPwd = (String)jsonObj.get("pwd");
+//        String getName = (String)jsonObj.get("name");
+//        String getEmail = (String)jsonObj.get("email");
+//        String getPhone = (String)jsonObj.get("phone");
+//        String getAddr = (String)jsonObj.get("addr");
+
+
+        //int resState = dao.(getId, getPwd);
+
+        PrintWriter out = response.getWriter();
+
+
+        EditMemberDAO dao = new EditMemberDAO();
+
+        // 현재 하나의 멤버 객체는 list에 있음
+        List<MemberVO> list = dao.MemInfo(getId);
+
+        JSONArray memberArray = new JSONArray();
+        for (MemberVO e : list){
+            JSONObject memberInfo = new JSONObject();
+            memberInfo.put("id", e.getId());
+            memberInfo.put("pwd", e.getPwd());
+            memberInfo.put("memName", e.getMemName());
+            memberInfo.put("email", e.getEmail());
+            memberInfo.put("phone", e.getPhone());
+            memberInfo.put("addr", e.getAddr());
+
+            memberArray.add(memberInfo);
+        }
+
+        System.out.println(memberArray);
+
+        out.print(memberArray);
+    }
 }
