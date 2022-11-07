0063237ca2c3ea3b02d884689fa7c32d71af9da5
silverbell <fds2236@naver.com>
2022-11-07 21:49:07 +0900

    은종/22.11.07

--- src/main/java/com/kh/miniservlet/dao/MemDAO.java
+++ src/main/java/com/kh/miniservlet/dao/MemDAO.java
@@ -1,35 +1,36 @@
-package com.kh.miniservlet.dao;
-
-import com.kh.miniservlet.common.Common;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-// 로그인한 ID의 이메일 정보 불러오기
-public class MemDAO {
-    private Connection conn = null;
-    private Statement stmt = null;
-    private ResultSet rs = null;
-    private PreparedStatement pstmt = null;
-    
-    public String memInfo(String id) {
-        String email = "NONE";
-        try {
-            conn = Common.getConnection();
-            stmt = conn.createStatement();
-            String sql = "SELECT MEM_TB WHERE ID = " + "'" +id + "'";
-            rs = stmt.executeQuery(sql);
-
-            while(rs.next()) {
-                email = rs.getString("EMAIL");
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        Common.close(pstmt);
-        Common.close(conn);
-        return email;
-    }
-}
+//package com.kh.miniservlet.dao;
+//
+//import com.kh.miniservlet.common.Common;
+//
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+//
+//// 로그인한 ID의 이메일 정보 불러오기
+//public class MemDAO {
+//    private Connection conn = null;
+//    private Statement stmt = null;
+//    private ResultSet rs = null;
+//    private PreparedStatement pstmt = null;
+//
+//    public String memInfo(String id) {
+//        String email = "NONE";
+//        try {
+//            conn = Common.getConnection();
+//            stmt = conn.createStatement();
+//            String sql = "SELECT MEM_TB WHERE ID = " + "'" +id+ "'";
+//            rs = stmt.executeQuery(sql);
+//
+//            while(rs.next()) {
+//                email = rs.getString("EMAIL");
+//            }
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//        Common.close(rs);
+//        Common.close(stmt);
+//        Common.close(conn);
+//        return email;
+//    }
+//}
--- src/main/java/com/kh/miniservlet/servlet/mypage/MemberServlet.java
+++ src/main/java/com/kh/miniservlet/servlet/mypage/MemberServlet.java
@@ -1,42 +1,42 @@
-package com.kh.miniservlet.servlet.mypage;
-
-import com.kh.miniservlet.common.Common;
-import com.kh.miniservlet.dao.MemDAO;
-import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Struct;
-
-@RestController
-@CrossOrigin(origins = "http://localhost:3000") // 3000번에 대한 cors 허용
-public class MemberServlet extends HttpServlet {
-    @PostMapping("/MemberServlet") // 이클립스의 서블렛파일에서 쓰는 @WebServlet("/LoginServlet")와 같은 기능
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        request.setCharacterEncoding("utf-8"); // 한글 깨짐 방지를 위해서 설정
-        StringBuffer sb = Common.reqStringBuff(request); // 요청 메시지 받기
-        JSONObject jsonObj = Common.getJsonObj(sb); // 요청 받은 메시지 JSON 파싱
-
-        String getId = (String)jsonObj.get("id");
-
-        MemDAO dao = new MemDAO();
-        String resEmail = dao.memInfo(getId); // 왜 boolean타입이 아니야? => 참과 거짓이 필요한게 아니라 id를 반환 받아야하니까!
-        System.out.println("resEmail : " + resEmail);
-
-        PrintWriter out = response.getWriter(); // => http요청에 대한 서블릿 응답
-        JSONObject resJson = new JSONObject(); // json 객체
-
-        if(resEmail.equals("NONE")) resJson.put("result", "NOK");
-        else {
-            resJson.put("result", "OK");
-            resJson.put("id", resEmail);
-        }
-        out.print(resJson);
-        }
-    }
+//package com.kh.miniservlet.servlet.mypage;
+//
+//import com.kh.miniservlet.common.Common;
+//import com.kh.miniservlet.dao.MemDAO;
+//import org.json.simple.JSONObject;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.sql.Struct;
+//
+//@RestController
+//@CrossOrigin(origins = "http://localhost:3000") // 3000번에 대한 cors 허용
+//public class MemberServlet extends HttpServlet {
+//    @PostMapping("/MemberServlet") // 이클립스의 서블렛파일에서 쓰는 @WebServlet("/LoginServlet")와 같은 기능
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        request.setCharacterEncoding("utf-8"); // 한글 깨짐 방지를 위해서 설정
+//        StringBuffer sb = Common.reqStringBuff(request); // 요청 메시지 받기
+//        JSONObject jsonObj = Common.getJsonObj(sb); // 요청 받은 메시지 JSON 파싱
+//
+//        String getId = (String)jsonObj.get("id");
+//
+//        MemDAO dao = new MemDAO();
+//        String resEmail = dao.memInfo(getId); // 왜 boolean타입이 아니야? => 참과 거짓이 필요한게 아니라 id를 반환 받아야하니까!
+//        System.out.println("resEmail : " + resEmail);
+//
+//        PrintWriter out = response.getWriter(); // => http요청에 대한 서블릿 응답
+//        JSONObject resJson = new JSONObject(); // json 객체
+//
+//        if(resEmail.equals("NONE")) resJson.put("result", "NOK");
+//        else {
+//            resJson.put("result", "OK");
+//            resJson.put("id", resEmail);
+//        }
+//        out.print(resJson);
+//        }
+//    }
