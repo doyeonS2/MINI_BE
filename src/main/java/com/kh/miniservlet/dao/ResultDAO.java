da817953abf0b3e856f4e98c6eba901eebfb3cb9
silverbell <fds2236@naver.com>
2022-11-09 18:17:06 +0900

    은종/22.11.09

--- src/main/java/com/kh/miniservlet/dao/ResultDAO.java
+++ src/main/java/com/kh/miniservlet/dao/ResultDAO.java
@@ -1,77 +1,48 @@
-//package com.kh.miniservlet.dao;
-//
-//import com.kh.miniservlet.common.Common;
-//import com.kh.miniservlet.vo.ItemVO;
-//
-//import java.sql.*;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class ResultDAO {
-//    private Connection conn = null;
-//    private Statement stmt = null;
-//    private ResultSet rs = null;
-//    private PreparedStatement pstmt = null;
-//
-//    // 검색 기능 1번째 방법
-////    public int researchCheck(String result) {
-////        int resState = 200; // 200 데이터 반환 ,300 결과값 없음
-////        try {
-////            conn = Common.getConnection();
-////            stmt = conn.createStatement(); // Statement 객체
-////            String sql = "SELECT * FROM PRO_TB WHERE PRO_KORNAME LIKE '%" + result + "%'";
-////            rs = stmt.executeQuery(sql); // select문이니까 executeQuery
-////
-////            while (rs.next()) {
-////                String sqlItem = rs.getString("PRO_KORNAME");
-////                String sqlPrice = rs.getString("PRICE");
-////                String sqlImg = rs.getString("IMG1");
-////
-////                System.out.println("Item : " + sqlItem);
-////                System.out.println("Price : " + sqlPrice);
-////                System.out.println("Img : " + sqlImg);
-////
-////                // 포스트맨으로 확인 => 300 출력됨..!
-////                if (result.equals(sqlItem)) {
-////                    resState = 200;
-////                } else {
-////                    resState = 300;
-////                }
-////            }
-////            Common.close(rs);
-////            Common.close(stmt);
-////            Common.close(conn);
-////
-////        } catch (Exception e) {
-////            e.printStackTrace();
-////        }
-////        return resState;
-////    }
-//
-//
-//    // 검색 기능 2번째 방법
-//    public List<ItemVO> researchCheck(String result) {
-//        List<ItemVO> list = new ArrayList<>();
+package com.kh.miniservlet.dao;
+
+import com.kh.miniservlet.common.Common;
+import com.kh.miniservlet.vo.ItemVO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultDAO {
+    private Connection conn = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
+    private PreparedStatement pstmt = null;
+
+    // 검색 기능 1번째 방법
+//    public List<> researchCheck(String result) {
+//        int resState = 200; // 200 데이터 반환 ,300 결과값 없음
 //        try {
 //            conn = Common.getConnection();
-//            stmt = conn.createStatement();
+//            stmt = conn.createStatement(); // Statement 객체
 //            String sql = "SELECT * FROM PRO_TB WHERE PRO_KORNAME LIKE '%" + result + "%'";
-//            rs = stmt.executeQuery(sql);
+//            rs = stmt.executeQuery(sql); // select문이니까 executeQuery
 //
 //            while (rs.next()) {
-//                String proCode = rs.getString("PRO_CODE");
-//                String brand = rs.getString("BRAND");
-//                String proName = rs.getString("PRO_NAME");
-//                Integer price = rs.getInt("PRICE");
-//                Date launDate = rs.getDate("LAUN_DATE");
+//                String sqlProcode = rs.getString("PRO_CODE");
+//                String sqlItem = rs.getString("PRO_NAME");
+//                String sqlKoItem = rs.getString("PRO_KORNAME");
+//                String sqlPrice = rs.getString("PRICE");
+//                String sqlImg = rs.getString("IMG1");
+//
+//                System.out.println("Procode : " + sqlProcode);
+//                System.out.println("Item : " + sqlItem);
+//                System.out.println("Item : " + sqlKoItem);
+//                System.out.println("Price : " + sqlPrice);
+//                System.out.println("Img : " + sqlImg);
 //
 //                ItemVO vo = new ItemVO();
-//                vo.setProCode(proCode);
-//                vo.setBrand(brand);
-//                vo.setProName(proName);
-//                vo.setPrice(price);
-//                vo.setLaunDate(launDate);
-//                list.add(vo);
+//
+//                // 포스트맨으로 확인 => 300 출력됨..!
+//                if (result.equals(sqlItem)) {
+//                    resState = 200;
+//                } else {
+//                    resState = 300;
+//                }
 //            }
 //            Common.close(rs);
 //            Common.close(stmt);
@@ -80,6 +51,46 @@
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-//        return list;
+//        return resState;
 //    }
-//}
+
+
+    // 검색 기능 2번째 방법
+    public List<ItemVO> researchCheck(String result) {
+        List<ItemVO> list = new ArrayList<>();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM PRO_TB WHERE PRO_KORNAME LIKE '%" + result + "%'" + "OR PRO_NAME LIKE '%" + result + "%'" + "OR BRAND LIKE '%" + result + "%'";
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String proCode = rs.getString("PRO_CODE");
+                String brand = rs.getString("BRAND");
+                String img1Path = rs.getString("IMG1");
+                String proKorName = rs.getString("PRO_KORNAME");
+                String proName = rs.getString("PRO_NAME");
+                Integer price = rs.getInt("PRICE");
+                Date launDate = rs.getDate("LAUN_DATE");
+
+                ItemVO vo = new ItemVO();
+
+                vo.setProCode(proCode);
+                vo.setImg1Path(img1Path);
+                vo.setProKorName(proKorName);
+                vo.setBrand(brand);
+                vo.setProName(proName);
+                vo.setPrice(price);
+                vo.setLaunDate(launDate);
+                list.add(vo);
+            }
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
--- src/main/java/com/kh/miniservlet/servlet/item/ResultServlet.java
+++ src/main/java/com/kh/miniservlet/servlet/item/ResultServlet.java
@@ -1,40 +1,55 @@
-//package com.kh.miniservlet.servlet.item;
-//
-//import com.kh.miniservlet.common.Common;
-//import com.kh.miniservlet.dao.ResultDAO;
-//import org.json.simple.JSONObject;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//import java.util.List;
-//
-//@RestController
-//@CrossOrigin(origins = "http://localhost:3000") // 3000번에 대한 cors 허용
-//
-//
-//
-//public class ResultServlet extends HttpServlet {
-//    @PostMapping("/ResultServlet") // 이클립스의 서블렛파일에서 쓰는 @WebServlet("/LoginServlet")와 같은 기능
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        request.setCharacterEncoding("utf-8"); // 한글 깨짐 방지를 위해서 설정
-//        StringBuffer sb = Common.reqStringBuff(request); // 요청 메시지 받기
-//        JSONObject jsonObj = Common.getJsonObj(sb); // 요청 받은 메시지 JSON 파싱
-//
-//        String getResult = (String) jsonObj.get("result");
-//
-//        ResultDAO dao = new ResultDAO();
-//        List resState = dao.researchCheck(getResult);
-//
-//        PrintWriter out = response.getWriter();
-//        JSONObject resJson = new JSONObject();
-//
-//        resJson.put("result", resState);
-//        out.print(resJson);
-//    }
-//}
+package com.kh.miniservlet.servlet.item;
+
+import com.kh.miniservlet.common.Common;
+import com.kh.miniservlet.dao.ResultDAO;
+import com.kh.miniservlet.vo.BoardVO;
+import com.kh.miniservlet.vo.ItemVO;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // 3000번에 대한 cors 허용
+
+
+
+public class ResultServlet extends HttpServlet {
+    @PostMapping("/ResultServlet") // 이클립스의 서블렛파일에서 쓰는 @WebServlet("/LoginServlet")와 같은 기능
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("utf-8"); // 한글 깨짐 방지를 위해서 설정
+        response.setCharacterEncoding("utf-8");
+        StringBuffer sb = Common.reqStringBuff(request); // 요청 메시지 받기
+        JSONObject jsonObj = Common.getJsonObj(sb); // 요청 받은 메시지 JSON 파싱
+
+        String getResult = (String) jsonObj.get("result");
+
+        ResultDAO dao = new ResultDAO();
+        List<ItemVO> list = dao.researchCheck(getResult);
+
+        PrintWriter out = response.getWriter();
+        JSONArray searchArray = new JSONArray();
+        for (ItemVO e : list) {
+            // 자바 객체 생성
+            JSONObject searchInfo = new JSONObject();
+            searchInfo.put("proCode", e.getProCode());
+            searchInfo.put("brand", e.getBrand());
+            searchInfo.put("proKorName", e.getProKorName());
+            searchInfo.put("img1Path", e.getImg1Path());
+
+            searchArray.add(searchInfo);
+        }
+        System.out.println(searchArray);
+        out.print(searchArray);
+    }
+}
